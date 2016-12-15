b62b1d4c501bbcf8f86025a7cb3ac3ee1b39ec92
My ASUS ZenBook Pro <jaytyoung1@gmail.com>
2016-12-15 12:53:38 -0500

    Press tab key to cycle through the five camera

---
+++ GameDevFinal/Assets/Scripts/CameraManager.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraManager : MonoBehaviour
+{
+    public int startingCamera = 0;
+    static int cameraIndex;
+    public Camera[] mainCameras;
+
+    void Start ()
+    {
+        cameraIndex = startingCamera;
+        updateCamera(cameraIndex);
+    }
+
+    void Update ()
+    {
+	    if (Input.GetKeyUp(KeyCode.Tab))
+            switchCamera(cameraIndex);
+    }
+
+    void updateCamera(int camera)
+    {
+
+        for (int i = 0; i < mainCameras.Length; i++)
+        {
+            if (i == camera)
+                mainCameras[i].enabled = true;
+            else
+                mainCameras[i].enabled = false;
+        }
+        Debug.Log("Displaying camera " + camera);
+    }
+
+    void switchCamera(int currentCamera)
+    {
+        switch (currentCamera)
+        {
+            case 4:
+                cameraIndex = 0;
+                updateCamera(cameraIndex);
+                break;
+            case 3:
+                cameraIndex++;
+                updateCamera(cameraIndex);
+                break;
+            case 2:
+                cameraIndex++;
+                updateCamera(cameraIndex);
+                break;
+            case 1:
+                cameraIndex++;
+                updateCamera(cameraIndex);
+                break;
+            case 0:
+                cameraIndex++;
+                updateCamera(cameraIndex);
+                break;
+        }
+    }
+}
