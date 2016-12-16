1ad726b7b40ae1d48431b52d7b9bad4612dc1135
My ASUS ZenBook Pro <jaytyoung1@gmail.com>
2016-12-16 16:02:47 -0500

    Toggle wind on/off by pressing w key

---
+++ GameDevFinal/Assets/Scripts/WindManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WindManager : MonoBehaviour
+{
+    public WindZone windZone;
+
+	// Use this for initialization
+	void Start ()
+    {
+        windZone.windMain = 0;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    if (Input.GetKeyUp(KeyCode.W))
+        {
+            if (windZone.windMain == 0)
+                windZone.windMain = 1;
+            else
+                windZone.windMain = 0;
+        }
+	}
+}
