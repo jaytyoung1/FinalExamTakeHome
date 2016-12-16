b05695d18409ec9e6cc0301d9274af9adeb332ac
My ASUS ZenBook Pro <jaytyoung1@gmail.com>
2016-12-16 00:41:12 -0500

    Toggle fog on/off by pressing f key

---
+++ GameDevFinal/Assets/Scripts/FogManager.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FogManager : MonoBehaviour
+{
+	void Update ()
+    {
+	    if (Input.GetKeyUp(KeyCode.F))
+            RenderSettings.fog = !RenderSettings.fog;
+	}
+}
